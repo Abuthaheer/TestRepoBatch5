251b5e3f4832784bc8be6c8883d87833bba39497
John Doe <john@sample.com>
2018-08-29 08:12:40 +0530

    Adding Files

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,12 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println("Its a Test Project for TRaining");
+	
+	
+	}
+
+}
